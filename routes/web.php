c05ab587f3e8ddbca47d63a3b5c8dad001f6f986
Fanis <fanis.kolokythas@gmail.com>
2022-09-04 13:19:04 +0300

    Update web.php
    removed test code

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 
 Route::group(['middleware' => ['auth']], function() {
    Route::get('/logout', [App\Http\Controllers\LogoutController::class, 'perform'])->name('logout.perform');

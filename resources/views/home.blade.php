eb2c318d8fda6abd9f25eb7e4baccfe750fee4c5
fanis-kol <72409285+fanis-kol@users.noreply.github.com>
2023-06-14 21:03:57 +0300

    books list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,16 @@
 @section('content')
 <div class="container">
     <div class="row">
-
+        @if(isset($books))
+            @foreach($books as $book)
+                <div class="col-12">
+                    <p class="name">{{$book->name}}</p>
+                    <p class="publisher">{{$book->publisher}}</p>
+                    <p class="author">{{$book->author}}</p>
+                    <p class="year">{{$book->year}}</p>
+                </div>
+            @endforeach
+        @endif
     </div>
    
 

a9bbf09ef36cd9085ceac65e7abcc60c25c7419b
fanis-kol <72409285+fanis-kol@users.noreply.github.com>
2024-02-24 20:25:43 +0200

    css and book cover

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -5,15 +5,18 @@
 
 <div class="container">
     <div class="row">
+        <div class="book-filters-wrap">
+            <input class="search" namme="search-input" class="search-input">
+        </div>
         @if(isset($books))
             @foreach($books as $book)
-            <div class="col-4">
+            <div class="col-12 col-md-6">
                 <div class="book-wrap">
-                    <p class="book-title text-uppercase">Title: {{$book->title}}</p>
-                    <p class="book-genre text-uppercase">Genre: {{$book->genre}}</p>
+                    <p class="book-title text-uppercase">{{$book->title}}</p>
+                    <p class="book-genre text-uppercase"> {{$book->genre}}</p>
                     <p class="book-pages text-uppercase">Pages: {{$book->pages}}</p>
                     <p class="author-name text-uppercase">Author: {{$book->author}}</p>
-                    <p class="publisher text-uppercase">Publisher: {{$book->publisher}}</p>
+                    <!-- <p class="publisher text-uppercase">Publisher: {{$book->publisher}}</p> -->
                 </div>
             </div>
              @endforeach
